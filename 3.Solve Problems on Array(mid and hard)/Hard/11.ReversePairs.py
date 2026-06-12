8103f5e9db3679634789e9300fd0e18e47a9da71
mokshapatel27 <mokshapatel.co23d1@scet.ac.in>
2026-06-12 19:39:56 +0530

    Create 11.ReversePairs.py
    Given an integer array nums, return the number of reverse pairs in the array.
    
    A reverse pair is a pair (i, j) where:
    
    0 <= i < j < nums.length and
    nums[i] > 2 * nums[j].

---
+++ 3.Solve Problems on Array(mid and hard)/Hard/11.ReversePairs.py
@@ -0,0 +1,23 @@
+#https://leetcode.com/problems/reverse-pairs/ LEFT
+
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        def merge_sort(left, right):
+            if left >= right:
+                return 0
+            
+            mid = (left + right) // 2
+            count = merge_sort(left, mid) + merge_sort(mid + 1, right)
+            
+            # Count the reverse pairs
+            j = mid + 1
+            for i in range(left, mid + 1):
+                while j <= right and nums[i] > 2 * nums[j]:
+                    j += 1
+                count += (j - (mid + 1))
+            
+            # In-place merge step to keep subarrays sorted
+            nums[left:right + 1] = sorted(nums[left:right + 1])
+            return count
+
+        return merge_sort(0, len(nums) - 1)
