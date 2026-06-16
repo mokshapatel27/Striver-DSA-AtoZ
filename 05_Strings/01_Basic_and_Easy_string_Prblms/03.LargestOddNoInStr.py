080c1848091416ac87fe2cddf00bdd8e1b610fce
mokshapatel27 <mokshapatel.co23d1@scet.ac.in>
2026-06-16 13:15:37 +0530

    Create 03.LargestOddNoInStr.py

---
+++ 05_Strings/01_Basic_and_Easy_string_Prblms/03.LargestOddNoInStr.py
@@ -0,0 +1,8 @@
+#https://leetcode.com/problems/largest-odd-number-in-string/
+
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        for i in range(len(num)-1,-1,-1):
+            if int(num[i])% 2!=0:
+                return num[:i+1]
+        return ""
