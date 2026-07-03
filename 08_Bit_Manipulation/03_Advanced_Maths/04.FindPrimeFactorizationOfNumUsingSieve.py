538d334792a6729fbd74b227134a2af95dc55b53
mokshapatel27 <mokshapatel.co23d1@scet.ac.in>
2026-07-03 11:05:59 +0530

    Update 04.FindPrimeFactorizationOfNumUsingSieve.py

--- 08_Bit_Manipulation/03_Advanced_Maths/04.FindPrimeFactorizationOfNumUsingSieve.py
+++ 08_Bit_Manipulation/03_Advanced_Maths/04.FindPrimeFactorizationOfNumUsingSieve.py
@@ -1,2 +1,22 @@
 #https://www.geeksforgeeks.org/problems/prime-factorization-using-sieve/1
 
+class Solution:
+
+    def findPrimeFactors(self, N):
+        # spf[i] will store the smallest prime factor of i
+        spf = [i for i in range(N + 1)]
+
+        # Standard Sieve to precompute SPF
+        for i in range(2, int(N**0.5) + 1):
+            if spf[i] == i:  # i is prime
+                for j in range(i * i, N + 1, i):
+                    if spf[j] == j:  # Mark only if not marked before
+                        spf[j] = i
+
+        # Logarithmic factorization using the precomputed SPF array
+        factors = []
+        while N > 1:
+            factors.append(spf[N])
+            N //= spf[N]
+
+        return factors
