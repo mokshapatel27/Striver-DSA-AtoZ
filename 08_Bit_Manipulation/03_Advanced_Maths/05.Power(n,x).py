81249f54b6d52d22ce50f63d7bea7e9f8c674a19
mokshapatel27 <mokshapatel.co23d1@scet.ac.in>
2026-07-03 11:07:19 +0530

    Create 05.Power(n,x).py

---
+++ 08_Bit_Manipulation/03_Advanced_Maths/05.Power(n,x).py
@@ -0,0 +1,15 @@
+#https://leetcode.com/problems/powx-n/description/
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n<0:
+            x=1/x
+            n=-n
+        ans=1.0
+        while n>0:
+            if n%2==1:
+                ans*=x
+
+            x*=x
+            n//=2
+        return ans
