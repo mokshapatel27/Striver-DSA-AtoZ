4b7758202d922dbceddb0cb6b7659fc03227fc79
mokshapatel27 <mokshapatel.co23d1@scet.ac.in>
2026-08-18 19:44:30 +0530

    Create 13.Preorder,Inorder,andPostorderTraversalinoneTraversa.py

---
+++ 13_Binary_Trees_[Traversals,_Medium_and_Hard_Problems]/01_Traversals/13.Preorder,Inorder,andPostorderTraversalinoneTraversa.py
@@ -0,0 +1,29 @@
+#https://www.naukri.com/code360/problems/tree-traversal_981269
+
+def getTreeTraversal(root):
+    if not root:
+        return [],[],[]
+
+    inorder,preorder,postorder=[],[],[]
+    stack=[[root,1]]
+
+    while stack:
+        it=stack[-1]
+
+        #preorder
+        if it[1]==1:
+            preorder.append(it[0].data)
+            it[1]+=1
+            if it[0].left:
+                stack.append([it[0].left,1])
+        elif it[1]==2:
+            
+            inorder.append(it[0].data)
+            it[1]+=1
+            if it[0].right:
+                stack.append([it[0].right,1])
+        else:
+            postorder.append(it[0].data)
+            stack.pop()
+
+    return [inorder,preorder,postorder]
